b80ed7a4e5a045d40f54ff970c08795ac2f353f1
Racquel Glickman <racquel.glickman@gmail.com>
2023-04-26 15:50:36 -0400

    wednesday afternoon, before routing

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-function Timer() {
+function Timer({ inGame, onGameStart }) {
+
+  /*
+    when inGame becomes true, start the timer
+    when submit is pressed on the GuessForm, change isGame to false
+  */
+
   return (
     <div id='timer'>
         <h1>Timer goes here</h1>
