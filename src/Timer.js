f17fceb6bfca992a153852cf07aae4a032707b06
Racquel Glickman <racquel.glickman@gmail.com>
2023-04-27 17:08:23 -0400

    both modals work

--- src/Timer.js
+++ src/Timer.js
@@ -1,40 +1,43 @@
 import {React, useEffect, useState, useContext} from 'react'
 import { MyContext } from './MyProvider';
-import Modal from './AnswerModal';
+import Modal from './TimerModal';
 
 function Timer() { 
 
  const { inGame, onStartGame, timeRemaining, onResetTime } = useContext(MyContext);
 
-function reset() {
-  onResetTime(10);
-  onStartGame(false);
-  alert(`time's up!`)
-}
+ const [isOpen, setIsOpen] = useState(false);
 
-useEffect(() => {
-
-  let interval = null;
-
-  if (inGame) {
-
-    interval = setInterval(() => {
-      onResetTime(timeRemaining => timeRemaining - 1);
-    }, 1000);
-
-    if (timeRemaining === -1) {
-      reset();
-    }
-
-  } else if (inGame === false && timeRemaining !== 0) {
-    clearInterval(interval);
+  function reset() {
+    onResetTime(10);
+    onStartGame(false);
+    setIsOpen(!isOpen);
   }
-  return () => clearInterval(interval);
+
+  useEffect(() => {
+
+    let interval = null;
+
+    if (inGame) {
+
+      interval = setInterval(() => {
+        onResetTime(timeRemaining => timeRemaining - 1);
+      }, 1000);
+
+      if (timeRemaining === -1) {
+        reset();
+      }
+
+    } else if (inGame === false && timeRemaining !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
 
 }, [inGame, timeRemaining]);
 
   return (
     <div id='timer'>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)} ></Modal>
         <h1>{timeRemaining}</h1>
     </div>
   )
