a6edca2e56509063f04af8d0b71464f2c5d6c9f5
Yinson Tso <yinson.tso@gmail.com>
2023-04-27 15:52:06 -0400

    Added Working Modal

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -16,7 +16,7 @@ const OVERLAY_STYLES = { //Makes background dark and unclickable
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0,0,0,.7)',
+    backgroundColor: 'rgba(0,0,0,.75)',
     zIndex: 1000
 }
 
@@ -27,6 +27,13 @@ function Modal({open, onClose, score}) {
     <>
         <div style={OVERLAY_STYLES}/> 
         <div style = {MODAL_STYLES}>
+            {((score.distancePercentError+score.durationPercentError)/2) < 65 ? <h2>Good Job</h2> : <h2>Do better{<p>( う-´)づ︻╦̵̵̿╤── \(˚☐˚”)/</p>}</h2>}
+            <p>Score:</p>
+            <h2>{score.overallScore}</h2>
+            <h5>Distance: {score.distance} Miles</h5>
+            <h5>Distance Guessed: {score.distanceGuess} Miles</h5>
+            <h5>Duration: {score.duration} Hours</h5>
+            <h5>Duration Guessed: {score.durationGuess} Hours</h5>
             <button onClick = {onClose}>Close</button>
         </div>
     </>
