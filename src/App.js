bf0b06da09b6678a4d23fcc8d58a254b71bfd50a
Yinson Tso <yinson.tso@gmail.com>
2023-06-06 17:57:41 -0400

    finilization

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import './App.css';
-import React, { useState } from 'react'
-import { Link, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import ScoreboardContainer from './ScoreboardContainer';
-import Questions from './Questions';
-import MyProvider from './MyProvider';
-import Error from './Error';
+import "./App.css";
+import React, { useState } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import ScoreboardContainer from "./ScoreboardContainer";
+import Questions from "./Questions";
+import MyProvider from "./MyProvider";
+import Error from "./Error";
 
 function App() {
-
   return (
     <MyProvider>
       <div className="App">
-
-        <div className='navbar'>
-          <img 
-            src='road-scholar-logo.png' 
-            alt='logo picture' 
-          ></img>
+        <div className="navbar">
+          <img src="road-scholar-logo.png" alt="logo picture"></img>
           <nav>
-            <Link className='link' to='/' > Home </Link>
-            <Link className='link' to='/questions' > Questions </Link>
-            <Link className='link' to='/scoreboard' > Scoreboard </Link>
+            <Link className="link" to="/">
+              {" "}
+              Home{" "}
+            </Link>
+            <Link className="link" to="/questions">
+              {" "}
+              Questions{" "}
+            </Link>
+            <Link className="link" to="/scoreboard">
+              {" "}
+              Scoreboard{" "}
+            </Link>
           </nav>
         </div>
-      
 
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/questions' element={<Questions />}/>
-          <Route path='/scoreboard' element={<ScoreboardContainer />}/>
-          <Route path='*' element={<Error />}/>
-        </Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/scoreboard" element={<ScoreboardContainer />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </div>
     </MyProvider>
-    
   );
-};
+}
 
 export default App;
